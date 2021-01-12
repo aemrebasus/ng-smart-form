8f7686626104d66afe70ba79e30aab1507d9f11e
Ahmet Emrebas <aemrebasus@gmail.com>
2021-01-11 21:07:25 -0600

    Submit Button is not activated when the form is ready! the problem was the touched condition which does not turn to true before deselecting the field closes #1

--- projects/ae-dynamic-form/src/lib/ae-dynamic-form.component.ts
+++ projects/ae-dynamic-form/src/lib/ae-dynamic-form.component.ts
@@ -1,53 +1,100 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
 import { InputType } from 'form-input-type';
-import { AeDynamicForm, AeFormBuilder, DateControlType } from './ae-dynamic-form.class';
+import {
+  AeDynamicForm,
+  AeFormBuilder,
+  DateControlType,
+} from './ae-dynamic-form.class';
 
-const TEXT_FIELD: InputType[] = ['text', 'color', 'number', 'email', 'password', 'search', 'tel', 'url'];
+const TEXT_FIELD: InputType[] = [
+  'text',
+  'color',
+  'number',
+  'email',
+  'password',
+  'search',
+  'tel',
+  'url',
+];
 const SELECT_FIELD: InputType[] = ['select', 'select-many'];
 const RANGE_FIELD: InputType[] = ['range'];
 const DATE_FIELD: InputType[] = ['date', 'datetime-local', 'week', 'time'];
 
-
 @Component({
   selector: 'ae-dynamic-form',
   templateUrl: './ae-dynamic-form.component.html',
-  styleUrls: ['./ae-dynamic-form.component.scss']
+  styleUrls: ['./ae-dynamic-form.component.scss'],
 })
 export class AeDynamicFormComponent implements OnInit {
-
   public formGroup: FormGroup;
   public isSubmitted$ = false;
-  public get isSubmitted(): boolean { return this.isSubmitted$; }
-  public set isSubmitted(value: boolean) { this.isSubmitted$ = value; }
+  public get isSubmitted(): boolean {
+    return this.isSubmitted$;
+  }
+  public set isSubmitted(value: boolean) {
+    this.isSubmitted$ = value;
+  }
 
   @Output() submitted = new EventEmitter<{ [key: string]: string }>();
 
   @Input() input: AeDynamicForm = new AeFormBuilder()
     .title('Form Title')
-    .newControl('firstName').placeholder('Type First Name').icon('360')
-    .label('First Name').required().maxLength(10).minLength(3).buildFormControl()
-
-    .newControl('lastName').placeholder('Type Last Name')
-    .icon('perm_camera_mic').required().label('Last Name').buildFormControl()
-
-    .newControl('dbirth').type('date').startDate(Date.now()).required().maxDate(Date.now()).buildFormControl()
-
-    .newControl('Select Option').type('select')
-    .options([{ label: 'Option 1', value: 'Value 1', }, { label: 'Option 2', value: 'Value 2', }, { label: 'Option 3', value: 'Value 3', }])
+    .newControl('firstName')
+    .placeholder('Type First Name')
+    .icon('360')
+    .label('First Name')
+    .required()
+    .maxLength(10)
+    .minLength(3)
     .buildFormControl()
 
-    .newControl('range').type('range').label('Range Label')
-    .range({ min: 0, max: 100 }).buildFormControl()
+    .newControl('lastName')
+    .placeholder('Type Last Name')
+    .icon('perm_camera_mic')
+    .required()
+    .label('Last Name')
+    .buildFormControl()
 
-    .newControl('gender').type('radio')
-    .options([{ label: 'male', value: 'male' }, { label: 'female', value: 'female' }]).buildFormControl()
+    .newControl('dbirth')
+    .type('date')
+    .startDate(Date.now())
+    .required()
+    .maxDate(Date.now())
+    .buildFormControl()
 
-    .newControl('Fruit').label('fruit').type('checkbox').buildFormControl()
+    .newControl('Select Option')
+    .type('select')
+    .options([
+      { label: 'Option 1', value: 'Value 1' },
+      { label: 'Option 2', value: 'Value 2' },
+      { label: 'Option 3', value: 'Value 3' },
+    ])
+    .buildFormControl()
 
-    .newControl('Banana').label('Banana').type('checkbox').buildFormControl()
+    .newControl('range')
+    .type('range')
+    .label('Range Label')
+    .range({ min: 0, max: 100 })
+    .buildFormControl()
 
+    .newControl('gender')
+    .type('radio')
+    .options([
+      { label: 'male', value: 'male' },
+      { label: 'female', value: 'female' },
+    ])
+    .buildFormControl()
 
+    .newControl('Fruit')
+    .label('fruit')
+    .type('checkbox')
+    .buildFormControl()
+
+    .newControl('Banana')
+    .label('Banana')
+    .type('checkbox')
+    .buildFormControl()
 
     .buildForm();
 
@@ -58,13 +105,13 @@ export class AeDynamicFormComponent implements OnInit {
   private initFormGroup(): void {
     const dateRangeHelper0 = new FormControl('');
     const object = {};
-    this.input.formInputs.forEach(i => {
-      if (i.dateType === 'range') {
+    for (const input of this.input.formInputs) {
+      if (input.dateType === 'range') {
         // tslint:disable-next-line: no-string-literal
         object['dateRangeHelper0'] = dateRangeHelper0;
       }
-      object[i.name] = new FormControl(i.state, i.validators);
-    });
+      object[input.name] = new FormControl(input.state, input.validators);
+    }
     this.formGroup = new FormGroup(object);
   }
 
@@ -72,7 +119,9 @@ export class AeDynamicFormComponent implements OnInit {
     return this.formGroup.controls[controlName];
   }
 
-  private getErrorsByControlName(controlName: string): { [key: string]: string | null } {
+  private getErrorsByControlName(
+    controlName: string
+  ): { [key: string]: string | null } {
     return this.getFormControlByName(controlName).errors;
   }
 
@@ -81,20 +130,38 @@ export class AeDynamicFormComponent implements OnInit {
     const errors = this.getErrorsByControlName(controlName);
     return errors ? Object.values(errors) : null;
   }
-  public isFormSubmitable():
-    boolean { return this.isFormFieldsValid() && this.isFormValid() && this.isFormTouched() && this.isFormDirty(); }
-  public isFormFieldsValid():
-    boolean { return Object.values(this.formGroup.controls).map(c => c.valid && c.touched && c.dirty).reduce((f, s) => f && s); }
-  public isFormValid(): boolean { return this.formGroup.valid; }
-  public isFormInvalid(): boolean { return this.formGroup.invalid; }
-  public isFormTouched(): boolean { return this.formGroup.touched; }
-  public isFormDirty(): boolean { return this.formGroup.dirty; }
-
+  public isFormSubmitable(): boolean {
+    return (
+      this.isFormFieldsValid() &&
+      this.isFormValid() &&
+      this.isFormTouched() &&
+      this.isFormDirty()
+    );
+  }
+  public isFormFieldsValid(): boolean {
+    return Object.values(this.formGroup.controls)
+      .map((c) => c.valid &&  c.dirty)  //c.touched &&
+      .reduce((f, s) => f && s);
+  }
+  public isFormValid(): boolean {
+    return this.formGroup.valid;
+  }
+  public isFormInvalid(): boolean {
+    return this.formGroup.invalid;
+  }
+  public isFormTouched(): boolean {
+    return this.formGroup.touched;
+  }
+  public isFormDirty(): boolean {
+    return this.formGroup.dirty;
+  }
 
   public selectedValueChange(inputName: string): void {
-    this.input.formInputs.forEach(e => {
+    this.input.formInputs.forEach((e) => {
       if (e.dynamicOptions && e.dynamicOptions.inputName === inputName) {
-        e.options = e.dynamicOptions.handler(this.getInputValueByName(inputName));
+        e.options = e.dynamicOptions.handler(
+          this.getInputValueByName(inputName)
+        );
       }
     });
   }
@@ -111,26 +178,49 @@ export class AeDynamicFormComponent implements OnInit {
       alert('Form is not valid!');
     }
   }
-  public reset(): void { this.formGroup.reset(); }
+  public reset(): void {
+    this.formGroup.reset();
+  }
 
-  public getInputValueByName(name: string): any { return this.getFormControlByName(name).value; }
-
-
+  public getInputValueByName(name: string): any {
+    return this.getFormControlByName(name).value;
+  }
 
   // Is Input type === ?
-  public isTextField(value: InputType | null): boolean { if (!value) { return true; } return TEXT_FIELD.includes(value); }
-  public isDateField(value: InputType | null): boolean { return DATE_FIELD.includes(value); }
-  public isBasicDateField(value: DateControlType): boolean { return value === 'basic' || value == null; }
-  public isRangeDateField(value: DateControlType): boolean { return value === 'range'; }
-  public isSelectField(type: InputType): boolean { return SELECT_FIELD.includes(type); }
-  public isMultipleSelect(type: InputType): boolean { return type === 'select-many'; }
-  public isTextArea(type: InputType): boolean { return type === 'text-area'; }
-  public isCheckbox(type: InputType): boolean { return type === 'checkbox'; }
-  public isRadio(type: InputType): boolean { return type === 'radio'; }
-  public isRange(type: InputType): boolean { return type === 'range'; }
+  public isTextField(value: InputType | null): boolean {
+    if (!value) {
+      return true;
+    }
+    return TEXT_FIELD.includes(value);
+  }
+  public isDateField(value: InputType | null): boolean {
+    return DATE_FIELD.includes(value);
+  }
+  public isBasicDateField(value: DateControlType): boolean {
+    return value === 'basic' || value == null;
+  }
+  public isRangeDateField(value: DateControlType): boolean {
+    return value === 'range';
+  }
+  public isSelectField(type: InputType): boolean {
+    return SELECT_FIELD.includes(type);
+  }
+  public isMultipleSelect(type: InputType): boolean {
+    return type === 'select-many';
+  }
+  public isTextArea(type: InputType): boolean {
+    return type === 'text-area';
+  }
+  public isCheckbox(type: InputType): boolean {
+    return type === 'checkbox';
+  }
+  public isRadio(type: InputType): boolean {
+    return type === 'radio';
+  }
+  public isRange(type: InputType): boolean {
+    return type === 'range';
+  }
 
   // public inComparisonDateField(value: DateControlType): boolean {    return value === 'comparison'; }
   // public isConstantRangeDateField(value: DateControlType): boolean {     return value === 'constant-range';   }
-
-
 }
